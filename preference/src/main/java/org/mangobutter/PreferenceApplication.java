e3169d7a003abfa75cf51f99e2d96328262f8359
edward.seufert <edward.seufert@TPA1WS01190.corp.autoclubfl.com>
2017-07-07 12:51:16 -0400

    test

---
+++ preference/src/main/java/org/mangobutter/PreferenceApplication.java
@@ -0,0 +1,12 @@
+package org.mangobutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreferenceApplication.class, args);
+	}
+}
